df23b749d314df1eb3d98098e59c44562b65c893
Hisham Najem <hnaje080@uottawa.ca>
2016-08-30 16:42:12 -0400

    Initializing project

---
+++ studentq/src/main/java/com/thefranchise/StudentqApplication.java
@@ -0,0 +1,12 @@
+package com.thefranchise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentqApplication.class, args);
+	}
+}
---
+++ studentq/src/test/java/com/thefranchise/StudentqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thefranchise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
